964bdfcdf37c9ee0a99e26af2681e0b5d0c920e5
nicominue <nicominue@gmail.com>
2021-12-11 14:37:55 -0300

    First commit

---
+++ integrador-A/app.js
@@ -0,0 +1,46 @@
+const countries = require('./countries.json');
+
+/** Get only the name of the country given his iso3 code.
+ * For example: given the isoCode 'ARG'
+ * The response should be: 'Argentina'
+ * @param  {array} isoCode
+ * @returns {array}
+ */
+function getCountryNameByIso3(isoCode){
+    //complete with your code
+}
+
+/**
+ * Get country summary given the ISO3 code. 
+ * It has to be a new object containing only the following data:
+ * {
+ *  name: ,
+ *  iso3: ,
+ *  iso2: ,
+ *  phone_code,
+ *  capital: ,
+ *  currency: ,
+ *  tld: ,
+ *  region: ,
+ *  emoji: ,
+ * }
+ * @param  {string} isoCode
+ * @returns {Object}
+ */
+function getCountrySummary(isoCode){
+    //complete with your code
+}
+
+function main() {
+    console.log(
+        '///// Ejercicio 1 /////\n',
+        getCountryNameByIso3('ARG'),
+        '\n');
+    console.log(
+        '///// Ejercicio 2 /////\n',
+        getCountrySummary('ARG'),
+        '\n');
+
+}
+
+main();
